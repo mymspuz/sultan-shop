44641d9b7da03f054b86834fec8d8d2bfae7e79b
mymspuz <mymspuz@gmail.com>
2023-04-03 09:04:01 +0300

    Change CSS

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
+import './sass/style.css'
 import { store } from './store'
 import MainRouter from './routes'
 
